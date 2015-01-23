9feb304ad7abbba828e94437283fa63102109737
Poschi <development@poschi.net>
2015-01-23 11:31:55 +0100

    Inital commit

---
+++ pumpe.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(25, GPIO.OUT)
+GPIO.setup(24, GPIO.OUT)
+GPIO.setup(23, GPIO.OUT)
+GPIO.output(25, 1)
+GPIO.output(24, 1)
+GPIO.output(23, 1)
+time.sleep(35)
+GPIO.output(25, 0)
+GPIO.output(24, 0)
+GPIO.output(23, 0)
+GPIO.cleanup()
+
