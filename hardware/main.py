3e832d48e0d43f8b110300a5021fab3435e7da31
Peter Neubauer <development@smeky.de>
2015-01-23 12:23:58 +0100

    Zwei weitere Pumpen hinzugefügt

--- hardware/main.py
+++ hardware/main.py
@@ -3,8 +3,12 @@ import PumpControl
 
 GPIO.setmode(GPIO.BCM)
 
-pumpA = PumpControl.PumpControl(25)
+pumpYellow = PumpControl.PumpControl(25)
+pumpGreen = PumpControl.PumpControl(24)
+pumpBlue = PumpControl.PumpControl(23)
 
-pumpA.runPump(5)
+pumpYellow.runPump(5)
+pumpBlue.runPump(1)
+pumpGreen.runPump(3)
 
 GPIO.cleanup()
