d57259091b67e5392dfce85b10612b7d8c36af20
Poschi <development@poschi.net>
2015-01-23 13:06:46 +0100

    Test für Async Aufruf

--- hardware/main.py
+++ hardware/main.py
@@ -10,9 +10,9 @@ pumpYellow = PumpControl.PumpControl(25)
 pumpGreen = PumpControl.PumpControl(24)
 pumpBlue = PumpControl.PumpControl(23)
 
-pool.apply_async(pumpYellow.runPump(5))
-pool.apply_async(pumpBlue.runPump(1))
-pool.apply_async(pumpGreen.runPump(3))
+pool.apply_async(pumpYellow.runPump, [5])
+pool.apply_async(pumpBlue.runPump, [1])
+pool.apply_async(pumpGreen.runPump, [3])
 
 GPIO.cleanup()
 
