5c1247d4170500c55c4671fe4e501504cdd275ad
Peter Neubauer <development@smeky.de>
2015-01-23 12:53:08 +0100

    Asynchrone Ansteuerung der Pumpen

--- hardware/main.py
+++ hardware/main.py
@@ -1,14 +1,21 @@
 import RPi.GPIO as GPIO
 import PumpControl
 
+from multiprocessing import Pool
+pool = Pool()
+
 GPIO.setmode(GPIO.BCM)
 
 pumpYellow = PumpControl.PumpControl(25)
 pumpGreen = PumpControl.PumpControl(24)
 pumpBlue = PumpControl.PumpControl(23)
 
-pumpYellow.runPump(5)
-pumpBlue.runPump(1)
-pumpGreen.runPump(3)
+pool.apply_async(pumpYellow.runPump, 5)
+pool.apply_async(pumpBlue.runPump, 1)
+pool.apply_async(pumpGreen.runPump, 3)
 
 GPIO.cleanup()
+
+
+
+
