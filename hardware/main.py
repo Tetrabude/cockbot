c1ba2235216a618ea916d1cda43f27d53daff568
Peter Neubauer <development@smeky.de>
2015-01-23 13:00:03 +0100

    Revert "Asynchrone Ansteuerung der Pumpen"
    This reverts commit 7fc1cbc146a1df2a658aee152a8761626cb187f9.

--- hardware/main.py
+++ hardware/main.py
@@ -1,21 +1,14 @@
 import RPi.GPIO as GPIO
 import PumpControl
 
-from multiprocessing import Pool
-pool = Pool()
-
 GPIO.setmode(GPIO.BCM)
 
 pumpYellow = PumpControl.PumpControl(25)
 pumpGreen = PumpControl.PumpControl(24)
 pumpBlue = PumpControl.PumpControl(23)
 
-pool.apply_async(pumpYellow.runPump, 5)
-pool.apply_async(pumpBlue.runPump, 1)
-pool.apply_async(pumpGreen.runPump, 3)
+pumpYellow.runPump(5)
+pumpBlue.runPump(1)
+pumpGreen.runPump(3)
 
 GPIO.cleanup()
-
-
-
-
