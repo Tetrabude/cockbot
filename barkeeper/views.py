741131b01629d020137887647fdaede3da050eca
Poschi <development@poschi.net>
2015-03-13 10:54:19 +0100

    Templates für index und details angelegt refactoring views

--- barkeeper/views.py
+++ barkeeper/views.py
@@ -1,12 +1,14 @@
-from django.http import HttpResponse
-from django.core.context_processors import request
+from django.shortcuts import render, get_object_or_404
+
+from django.http import HttpResponse, Http404
 from barkeeper.models import Recipe
 
 def index(request):
     recipe_list = Recipe.objects.order_by('name')
-    output = '<br>'.join([p.name for p in recipe_list])
-    return HttpResponse(output)
+    context = {'recipe_list': recipe_list}
+    return render(request, 'barkeeper/index.html', context)
 
 def detail(request, recipe_id):
-    return HttpResponse("Details zu Cocktail %s." % recipe_id)
+    recipe = get_object_or_404(Recipe, pk=recipe_id)
+    return render(request, 'barkeeper/detail.html', {'recipe': recipe})
 
