765e08f975130774caa35777dc1acea5774b83ce
Poschi <development@poschi.net>
2015-03-13 10:21:48 +0100

    Index zeigt jetzt Rezepte an Mehr Rezepte hinzugefügt

--- barkeeper/views.py
+++ barkeeper/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
 from django.core.context_processors import request
+from barkeeper.models import Recipe
 
 def index(request):
-    return HttpResponse("Cocktailliste")
+    recipe_list = Recipe.objects.order_by('name')
+    output = '<br>'.join([p.name for p in recipe_list])
+    return HttpResponse(output)
 
 def detail(request, recipe_id):
     return HttpResponse("Details zu Cocktail %s." % recipe_id)
