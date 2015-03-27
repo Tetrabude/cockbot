64aef18eaadcb9daf316d3615c1670af263e28e8
Peter Neubauer <development@smeky.de>
2015-03-27 10:38:10 +0100

    Added ExtraIngredient

--- barkeeper/admin.py
+++ barkeeper/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from barkeeper.models import Pump, RawMaterial, Recipe, Ingredient
+from barkeeper.models import Pump, RawMaterial, Recipe, Ingredient, ExtraIngredient
 
 admin.site.register(Pump)
 admin.site.register(RawMaterial)
@@ -7,8 +7,13 @@ admin.site.register(RawMaterial)
 class IngredienInline(admin.StackedInline):
     model = Ingredient
     extra = 3
+
+class ExtraIngredienInline(admin.StackedInline):
+    model = ExtraIngredient
+    extra = 3
     
+
 class RecipeAdmin(admin.ModelAdmin):
-    inlines = [IngredienInline]
+    inlines = [IngredienInline, ExtraIngredienInline]
     
 admin.site.register(Recipe, RecipeAdmin)
