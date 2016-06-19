f2e3d49cca43a70dffc69ffc0187e0f360bb2ea1
Poschi <development@poschi.net>
2016-06-19 20:41:00 +0200

    Redirect from / to /barkeeper

--- cockbot/urls.py
+++ cockbot/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 urlpatterns = patterns('',
     # Examples:
@@ -8,5 +9,5 @@ urlpatterns = patterns('',
 
     url(r'^barkeeper/', include('barkeeper.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    
+    url(r'^$', RedirectView.as_view(url='/barkeeper', permanent=True)),
 )
